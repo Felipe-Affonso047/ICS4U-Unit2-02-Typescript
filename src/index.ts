6aad6d611fae4440d4107e74770dd4d59b952a7f
Ubuntu <ubuntu@ip-172-31-89-242.ec2.internal>
2021-12-06 02:32:32 +0000

    linted

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,32 @@
 * @since   2021-12-05
 */
 
-import * as readline from 'readline';
+import * as readline from 'readline'
 
-function factorial(denominator: number) {
-    // this function is a factorial calculator.
-    if (denominator == 0)
-    {
-        return 1;
-    }
-    else if (denominator > 0)
-    {
-        var returnValue: number = denominator * factorial(denominator - 1);
-        return returnValue;
-    }
-    else
-    {
-        var returnValue: number = denominator * factorial(denominator + 1);
-        return returnValue;
-    }
+function factorial (denominator: number) {
+  // this function is a factorial calculator.
+  if (denominator === 0) {
+    return 1
+  } else if (denominator > 0) {
+    const returnValue1: number = denominator * factorial(denominator - 1)
+    return returnValue1
+  } else {
+    const returnValue2: number = denominator * factorial(denominator + 1)
+    return returnValue2
+  }
 }
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
-rl.question("Input an integer: ", function(answer) {
-    var factorialNum: number = factorial(parseInt(answer));
-    console.log("The factorial of " + answer + " is " + factorialNum);
-    rl.close();
-});
+rl.question('Input an integer: ', function (answer) {
+  try {
+    const factorialNum: number = factorial(parseInt(answer))
+    console.log('The factorial of ' + answer + ' is ' + factorialNum)
+  } catch (exeption) {
+    console.log('please insert an integer.')
+  }
+  rl.close()
+})
